f070bcb7397af132a743451cf936b3d0bd9dd9b2
Javohir0102 <javohiranvarov96@gmail.com>
2024-07-05 09:26:32 +0500

    Add project files.

---
+++ CalculatorNumberAndLetter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CalculatorNumberAndLetter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Presentation();
+        }
+
+        private static void Presentation()
+        {
+            Print("Dasturga hush kelibsiz");
+            Console.Write("matn kiriting: ");
+            string text = Console.ReadLine();
+
+            Print("Calculating!!!");
+            Print($"Matndagi raqamlar soni: {Services.DigitsOnly(text)}");
+            Print($"Matndagi harflar soni:  {Services.LettersOnly(text)}");
+            Print($"Boshqa belgilar soni:   {Services.OtherCharacters(text)}");
+        }
+
+        static void Print(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ CalculatorNumberAndLetter/Services.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CalculatorNumberAndLetter
+{
+    internal class Services
+    {
+        public static int DigitsOnly(string str)
+        {
+            int count = 0;
+            foreach (char c in str)
+            {
+                if (char.IsDigit(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public static int LettersOnly(string str)
+        {
+            int count = 0;
+            foreach (char c in str)
+            {
+                if (char.IsLetter(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public static int OtherCharacters(string str)
+        {
+            int count = 0;
+            foreach (char c in str)
+            {
+                if (!char.IsLetterOrDigit(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
